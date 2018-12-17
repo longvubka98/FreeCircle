b6c053df61a9ad8400eb7da442c2dc19f054146b
Vũ Long Vũ <longvubka98@gmail.com>
2018-12-18 03:30:47 +0700

    tao xong form va lay anh

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,26 +11,6 @@ import {
 
 import Logo from '../components/Logo';
 import {firebaseApp} from '../config';
-// import '@firebase/auth'
-// import "@firebase/database"
-// import '@firebase/firestore'
-// import '@firebase/storage'
-// import '@firebase/messaging'
-// import '@firebase/functions'
-
-// var config = {
-//   apiKey: "AIzaSyAJUaKjR44WnojFnPNf8u8QL5n0rsqRVW8",
-//   authDomain: "freecircle-56dc0.firebaseapp.com",
-//   databaseURL: "https://freecircle-56dc0.firebaseio.com",
-//   projectId: "freecircle-56dc0",
-//   storageBucket: "freecircle-56dc0.appspot.com",
-//   messagingSenderId: "387442449911"
-// };
-// if (!firebase.apps.length) {
-//   const firebaseApp = firebase.initializeApp(config);
-// }
-// import firebaseApp from '../config'
-
 export default class Login extends Component {
   constructor(props) {
     super(props);
--- src/pages/Mine.js
+++ src/pages/Mine.js
@@ -1,6 +1,8 @@
+import ImagePicker from 'react-native-image-crop-picker';
 import React, { Component } from 'react';
-import { Text, View, Image, TouchableOpacity,StyleSheet } from 'react-native';
-
+import { Text, View, Image, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
+import { ScrollView } from 'react-native-gesture-handler';
 export default class TransactionHistoryComponent extends Component {
   static navigationOptions = ({ navigation }) => {
     const { params = {} } = navigation.state;
@@ -13,32 +15,89 @@ export default class TransactionHistoryComponent extends Component {
     );
     return { tabBarLabel, tabBarIcon };
   }
-  
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: null,
+      images: null
+    };
+  }
+  pickImage() {
+    ImagePicker.openPicker({
+      // multiple: true,
+      width: 500,
+      height: 500,
+    }).then(image => {
+      this.setState({
+        image: { uri: image.path, width: image.width, height: image.height, mime: image.mime },
+        images: null
+        // images.map(i => {
+        //   console.log('received image', i);
+        //   return { uri: i.path, width: i.width, height: i.height, mime: i.mime };
+        // })
+      });
+    }).catch(e => Alert.alert(e));
+  }
+  renderImage(image) {
+    return <Image style={{ width: 300, height: 300, resizeMode: 'contain' }} source={image} />
+  }
   render() {
     return (
-      <View style={{
+      <ScrollView style={{
+        marginTop:40,
         flex: 1,
-        backgroundColor: '#964f8e',
+        backgroundColor: '#fff',
+      }}
+      contentContainerStyle = {{
         alignItems: 'center',
         justifyContent: 'center'
-        }}
+      }}
       >
-        <Text style={{ fontWeight: 'bold', fontSize: 22, color: 'white' }}>Đồ bạn đăng</Text>
+        <Text style={{ fontSize: 22, color: 'black' }}>Đồ bạn đăng</Text>
+        <FormLabel labelStyle={styles.formLabel}>TÊN BẠN</FormLabel>
+        <FormInput inputStyle={styles.formInput} containerStyle = {styles.formContai} placeholder="Name..."/>
+        <FormLabel labelStyle={styles.formLabel}>ĐỊA CHỈ</FormLabel>
+        <FormInput inputStyle={styles.formInput} containerStyle = {styles.formContai} placeholder="Địa chỉ..."/>
+        <FormLabel labelStyle={styles.formLabel}>SĐT LIÊN HỆ</FormLabel>
+        <FormInput inputStyle={styles.formInput} containerStyle = {styles.formContai}  placeholder="SĐT..." keyboardType='phone-pad'/>
+        <FormLabel labelStyle={styles.formLabel}>MÔ TẢ</FormLabel>
+        <FormInput inputStyle={styles.formInput1} containerStyle = {styles.formContai}/>
+        {this.state.image ? this.renderImage(this.state.image) : null}
+        <Text style={{ fontWeight: 'bold', fontSize: 12, color: 'white' }}></Text>
         <TouchableOpacity style={styles.button}
-        onPress = {() => this.props.navigation.navigate('DangDo')} >
-        <Text style = {styles.buttonText}>Chia sẻ đồ</Text>
+          onPress={() => this.pickImage()} >
+          <Text style={styles.buttonText}>Chia sẻ đồ</Text>
         </TouchableOpacity>
-      </View>
+      </ScrollView>
     );
   }
 }
 const styles = StyleSheet.create({
+  formLabel: {
+    color: '#000',
+    alignItems: 'flex-end'
+  },
+  formInput: {
+    color: '#000',
+    marginBottom: -15,
+    textAlign:'left'
+  },
+  formInput1: {
+    color: '#000',
+    paddingBottom:80,
+    marginBottom: -15,
+    textAlign:'left'
+  },
+  formContai:{
+    borderBottomWidth:1
+  },
   button: {
     width: 300,
     backgroundColor: '#138a72',
     borderRadius: 25,
     marginVertical: 10,
-    paddingVertical: 13
+    paddingVertical: 13,
   },
   buttonText: {
     fontSize: 16,
